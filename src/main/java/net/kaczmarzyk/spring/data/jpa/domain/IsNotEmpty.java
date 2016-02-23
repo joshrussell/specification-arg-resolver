d35849c62c46d92825ccd37e762a19c01f45a34b
Adrian Jagnanan <adrian.jagnanan@riptidecloud.com>
2016-02-23 12:10:38 -0500

    Add 3 new specifications: isEmpty, isNotEmpty, isNotNull

---
+++ src/main/java/net/kaczmarzyk/spring/data/jpa/domain/IsNotEmpty.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+
+/**
+ * <p>Filers with "is empty" where clause (e.g. {@code where nickName is empty}).</p>
+ * 
+ * <p>Does not require any http-parameters to be present, i.e. represents constant part of the query.</p>
+ * 
+ * @author Tomasz Kaczmarzyk
+ */
+public class IsNotEmpty<T> extends PathSpecification<T> implements ZeroArgSpecification {
+
+	public IsNotEmpty(String path, String[] args) {
+		super(path);
+	}
+
+	@Override
+	public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		return cb.isNotEmpty(this.<Collection>path(root));
+	}
+
+}
